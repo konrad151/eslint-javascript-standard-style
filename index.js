7bc9a5bc8c142f741e6b40892cddb86451e67711
Konrad <konrad.k151@gmail.com>
2018-11-25 23:21:18 +0100

    ESLint with JavaScript Standard Style extension

---
+++ index.js
@@ -0,0 +1,25 @@
+// // Use 2 spaces for indentation.
+// function hello (name) {
+// console.log('buła łokieć', name)
+// }
+// hello('rekinie')
+
+// // Use single quotes for strings except to avoid escaping.
+// console.log("mój wuja");
+
+// // No unused variables
+// function myFunction () {
+// 	var result = 5 // ✗ avoid
+// }
+
+// // Add a space after keywords
+// var b = 2
+// if(b === 2) {
+// 	console.log('jesteś mega luj')
+// }
+
+// // // Add a space before a function declaration's parentheses
+// function name() {
+// 	return 5
+// }
+// name()
